1a796be8e5d8ac99624d8672f77af646266eaa7d
李宇登 <li@maichong.it>
2020-08-27 09:34:26 +0800

    R

--- lib/r.dart
+++ lib/r.dart
@@ -1,14 +1,16 @@
 class R {
-  /// ![](http://127.0.0.1:2227/images/icon_image.png)
+  /// images/icon_image.png
   static final String imagesIconImagePng = 'images/icon_image.png';
-  /// ![](http://127.0.0.1:2227/images/icon_play.png)
+  /// images/icon_play.png
   static final String imagesIconPlayPng = 'images/icon_play.png';
-  /// ![](http://127.0.0.1:2227/images/icon_text.png)
+  /// images/icon_text.png
   static final String imagesIconTextPng = 'images/icon_text.png';
-  /// ![](http://127.0.0.1:2227/images/icon_video.png)
+  /// images/icon_video.png
   static final String imagesIconVideoPng = 'images/icon_video.png';
-  /// ![](http://127.0.0.1:2227/images/icon_home.png)
+  /// images/pic_default.png
+  static final String imagesPicDefaultPng = 'images/pic_default.png';
+  /// images/icon_home.png
   static final String imagesIconHomePng = 'images/icon_home.png';
-  /// ![](http://127.0.0.1:2227/images/icon_gif.png)
+  /// images/icon_gif.png
   static final String imagesIconGifPng = 'images/icon_gif.png';
 }
